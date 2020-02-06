8e2daae08d322d82bf8800ab4513fb1e6ce9a5ba
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-06 13:10:31 -0500

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeGlyphs = document.getElementsByClassName('like-glyph')
+const error = document.querySelector('#modal')
 
+function makeErrorHidden () {
+  error.classList.add('hidden') 
+}
 
-
+function addListenersToLike() {
+  for (const glyph of likeGlyphs) {
+    glyph.addEventListener('click', (e) => {
+      e.preventDefault 
+      if (glyph.innerHTML === EMPTY_HEART) {
+        mimicServerCall()
+        .then((response) => {
+          e.target.innerHTML = FULL_HEART
+          e.target.classList.add('activated-heart')
+        })
+        .catch((message) => {
+          error.classList.remove('hidden')
+          error.innerText = message
+          setTimeout(makeErrorHidden, 5000)
+        })
+      } else if (e.target.innerHTML === FULL_HEART) {
+        e.target.innerHTML = EMPTY_HEART
+        e.target.classList.remove('activated-heart')
+      }
+    })
+  }
+}
+makeErrorHidden()
+addListenersToLike()
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
